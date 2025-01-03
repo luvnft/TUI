f4082f2effa056cd7ed1d0509c4381bbda4dcce2
The Wizard of Hahz <info@luvnft.com>
2025-01-03 12:49:43 -0500

    up

--- iOS/Example/App/Main/MainViewController.swift
+++ iOS/Example/App/Main/MainViewController.swift
@@ -89,7 +89,7 @@ extension MainViewController {
     private func setupNavigation() {
         let logoImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
         logoImageView.contentMode = .scaleAspectFit
-        logoImageView.image = UIImage(named: "luv_nft_live")
+        logoImageView.image = UIImage(named: "luv_nft_live_webp")
         let logoImageItem = UIBarButtonItem(customView: logoImageView)
         
         let logoTitleItem = UIBarButtonItem(title: "TUILiveKit", style: .plain, target: nil, action: nil)
